2d39a0a8fc9b60b7933e5be6949ae067db643034
Niels Lubbes <niels.lubbes@noadds>
2018-04-07 12:26:18 +0200

    updated docs

--- orbital/src/orbital/prod/orb_product.py
+++ orbital/src/orbital/prod/orb_product.py
@@ -2,6 +2,9 @@
 Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
 Created on Jul 8, 2016
 @author: Niels Lubbes
+
+
+The main functionality of this module is accessed via the method orb_product()
 '''
 
 import warnings
