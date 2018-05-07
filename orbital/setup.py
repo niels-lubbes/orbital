730204203f4b21d23688b7c0f2f234ee72bef65f
Niels Lubbes <niels.lubbes@noadds>
2018-05-07 13:46:43 +0200

    Updating configuration for Pypi upload.

--- orbital/setup.py
+++ orbital/setup.py
@@ -11,29 +11,37 @@ https://pypi.python.org/pypi?%3Aaction=list_classifiers
 from setuptools import setup
 
 
-setup( name = 'orbital',
-       version = '0',
-       description = 'Python library for constructing and visualizing curves on surfaces',
-       classifiers = [
-           'Development Status :: 3 - Alpha',
-           'License :: OSI Approved :: MIT License',
-           'Programming Language :: Python :: 2',
-           'Programming Language :: Python :: 3',
-           'Topic :: Scientific/Engineering :: Mathematics',
-           ],
-      keywords = 'curves surfaces parametrization Povray',
-      url = 'http://github.com/niels-lubbes/orbital',
-      author = 'Niels Lubbes',
-      license = 'MIT',
-      package_dir = {'': 'src'},
-      packages = ['orbital'],
-      install_requires = ['linear_series'],
-      test_suite = 'nose.collector',
-      tests_require = ['nose'],
-      entry_points = {
-          'console_scripts': ['run-lattice=orbital.__main__:main'],
-      },
-      include_package_data = True,
-      zip_safe = False )
-
+setup( 
+    name = 'orbital',
+    version = '0',
+    description = 'Python library for constructing and visualizing curves on surfaces',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+    keywords = 'curves surfaces parametrization Povray',
+    url = 'http://github.com/niels-lubbes/orbital',
+    author = 'Niels Lubbes',
+    license = 'MIT',
+    package_dir = {
+        'orbital': 'src/orbital',
+        'cossin': 'src/orbital/cossin',
+        'pov': 'src/orbital/pov',
+        'povray': 'src/orbital/povray',
+        'prod': 'src/orbital/prod',
+    },
+    packages = ['orbital', 'cossin', 'pov', 'povray', 'prod'],
+    # include_package_data = True,
+    package_data = {'orbital': ['orb_tools.sobj'], 'cossin':['cos_sin.txt']},
+    install_requires = ['linear_series'],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+    entry_points = {
+        'console_scripts': ['run-lattice=orbital.__main__:main'],
+    },
+    zip_safe = False
+    )
 
